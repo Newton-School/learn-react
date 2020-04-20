f67a2ec9c1ae77fca0fb60c231d43206d86f9910
Rahul Shetty <rahul.shetty@here.com>
2020-04-20 18:43:30 +0530

    Add th default styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 // import Todo from "./containers/Todo/Todo";
-import employeesData from "./containers/EmployeesData/EmployeesData";
-import EmployeesBelowFifty from "./components/EmployeesBelowFifty/EmployeesBelowFifty";
-import EmployeesAboveLakh from "./components/EmployeesAboveLakh/EmployeesAboveLakh";
+// import employeesData from "./containers/EmployeesData/EmployeesData";
+// import EmployeesBelowFifty from "./components/EmployeesBelowFifty/EmployeesBelowFifty";
+// import EmployeesAboveLakh from "./components/EmployeesAboveLakh/EmployeesAboveLakh";
+import Portal from "./containers/Portal/Portal";
 import "./styles/base.css";
 
-const EmployeesBelowFiftyComp = employeesData(EmployeesBelowFifty);
+// const EmployeesBelowFiftyComp = employeesData(EmployeesBelowFifty);
 
-const EmployeesAboveLakhComp = employeesData(EmployeesAboveLakh);
+// const EmployeesAboveLakhComp = employeesData(EmployeesAboveLakh);
 
 function App() {
-  return (
-    <div style={{ display: "flex" }}>
-      <EmployeesBelowFiftyComp label="Add Submit" />
-      <EmployeesAboveLakhComp />
-    </div>
-  );
+  return <Portal />;
 }
 
 export default App;
