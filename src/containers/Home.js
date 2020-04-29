4229c17278f7eff1aeac31eebd3a9c0cd6386fb4
Rahul Shetty <rahul.shetty@here.com>
2020-04-29 23:02:40 +0530

    Display the counter value using setState

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {});
+
+  return (
+    <div>
+      <p>Counter: {counter}</p>
+
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        Update Counter
+      </button>
+    </div>
+  );
+};
 
 export default Home;
