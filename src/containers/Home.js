821cda35b471544f588d3bae8bc29c2043acfcac
Rahul Shetty <rahul.shetty@here.com>
2020-04-30 21:34:36 +0530

    Using useEffect

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,47 @@
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  const [counter, setCounter] = useState(0);
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
-  useEffect(() => {});
+  // Renders everytime
+  useEffect(() => {
+    console.log("Renders everytime");
+
+    return () => {
+      console.log("component will unmount");
+    };
+  });
+
+  // Renders once. Equivalent to componentDidMount
+  useEffect(() => {
+    console.log("Renders once");
+  }, []);
+
+  // Render only when counter1 changes
+  useEffect(() => {
+    console.log("Render only when counter1 changes");
+  }, [counter1]);
 
   return (
     <div>
-      <p>Counter: {counter}</p>
+      <div>
+        <p>Counter1: {counter1}</p>
 
-      <button type="button" onClick={() => setCounter(counter + 1)}>
-        Update Counter
-      </button>
+        <button type="button" onClick={() => setCounter1(counter1 + 1)}>
+          Update Counter
+        </button>
+      </div>
+
+      <br />
+
+      <div>
+        <p>Counter2: {counter2}</p>
+
+        <button type="button" onClick={() => setCounter2(counter2 + 1)}>
+          Update Counter
+        </button>
+      </div>
     </div>
   );
 };
