31da2d7f3fae5c21187d7821f8d7ffcd57c76247
IBRAHIM-KHA <88272470+IBRAHIM-KHA@users.noreply.github.com>
2021-08-13 14:06:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import "./styles/base.css";
 
 function App() {
   return (
-    <StateProvider>
-      <Navigation />
-    </StateProvider>
-  );
+    <div className="App">
+      <button id="click" onClick={handleclick}></button>
+      <p id="para"></p>
+    </div>
+  )
+    function handleclick()
+      {
+      //let htc1=document.createElement('p');
+     // htc1.id="para";
+     let htc=document.getElementById("para");
+      htc.innerHTML="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  }
 }
 
 export default App;
